c5fa6033102c4e185df01b031fed5793be2c7477
S <ssimac@SdeiMac.fritz.box>
2021-10-09 10:27:56 +0200

    deprecation

--- src/schema.js
+++ src/schema.js
@@ -36,13 +36,15 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "The track's complete array of Modules"
     modules: [Module!]!
   }
@@ -62,11 +64,13 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
   }
 `;
 
